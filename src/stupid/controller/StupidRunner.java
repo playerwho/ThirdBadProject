4552b423ec34bc521112e7b3870fe8004df76cea
Austin Widmeier <austinwidmeier@gmail.com>
2015-02-20 13:21:34 -0700

    hiii
    sasasasasa

---
+++ src/stupid/controller/StupidRunner.java
@@ -0,0 +1,12 @@
+package stupid.controller;
+
+public class StupidRunner
+{
+
+	public static void main(String[] args)
+	{
+		ThirdStupid myStupid = new ThirdStupid();
+		myStupid.start();
+	}
+
+}
---
+++ src/stupid/controller/ThirdStupid.java
@@ -0,0 +1,19 @@
+package stupid.controller;
+
+public class ThirdStupid
+{
+
+	public void start()
+	{
+		introduction();
+
+	}
+
+	private void introduction()
+	{
+		System.out.println("Hello, I'm Austin! - I went to work yesterday. ");
+		System.out.println("Sleeping is also a good one. ");
+		System.out.println("I work at a pizza place. ");
+		System.out.println("Walking dead is intense. ");
+	}
+}
